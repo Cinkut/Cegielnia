8048d8701c118b0f02d0291bcc070814f79011c6
Cinkut <lukaszmarcin053@gmail.com>
2025-02-21 17:13:16 +0100

    Male zmiany

--- Funkcje/ZmienneSrodowiskowe.h
+++ Funkcje/ZmienneSrodowiskowe.h
@@ -6,11 +6,11 @@
 
 #define N 4              // Liczba Ciężarówek
 #define C 20            // Ładowność każdej z Ciężarówek wyrażaone w liczbie Cegieł
-#define T 10           // Czas pod którym ciężarówce udaje się rozwieść cegły i wrócić do cegielni
+#define T 0           // Czas pod którym ciężarówce udaje się rozwieść cegły i wrócić do cegielni
 
 #define K 7             // Maksymalna ilość cegieł jaką może przetransportować naraz taśma
 #define M 19            // Maksymalna łączna masa cegieł jaką może przetransportować naraz taśma
 
-#define T1 1           // Czas co ktory wrzucaja cegly
+#define T1 0           // Czas co ktory wrzucaja cegly
 
 #define LiczbaPracownikow 3
